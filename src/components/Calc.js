c92f2ac9bf07a0004898c204ec0add60a22a6f5c
OriSass <67857592+OriSass@users.noreply.github.com>
2020-10-05 15:59:57 +0300

    challenge finished

--- src/components/Calc.js
+++ src/components/Calc.js
@@ -27,6 +27,7 @@ function calculate(operation, num1, num2 = 0) {
   }
 }
 
+
 function Calc() {
   /**
    * Add (0-9) to <DigitButton /> with value and onClick function as exlplained in the requirements
@@ -34,21 +35,69 @@ function Calc() {
    * This is a state machine, you'll need to work wisely with React.js State and Lifecycle functionality
    * You can use calculate function for your aid
    */
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
+  const [mathAction, setMathAction] = useState();
 
+  const buttons = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const addToNumber1 = (digitOrOperation) => setNumber1(prev => prev + digitOrOperation);
+  const addToNumber2 = (digitOrOperation) => setNumber2(prev => prev + digitOrOperation);
+  const reset = () => {
+    setNumber1('');
+    setNumber2('');
+    setMathAction('');
+  }
+  const handleEqualize = () => {
+    let result;
+    // debugger;
+    console.log(mathAction);
+    if(mathAction === '√'){
+      result = calculate('sqrt', parseInt(number2));
+    }
+    else if(mathAction === 'x²'){
+      result = calculate('power', parseInt(number1));
+    }
+    else {
+      result = calculate(mathAction, parseInt(number1), parseInt(number2));
+      if(result === Infinity){
+         result = 'Error'
+      };
+    }
+    reset();
+    setNumber1(result);
+  }
   return (
     <div className='calculator'>
       <div className='result'>
-        {/**
-         * Print the result of the calculation here
-         */}
+        {number1} {mathAction} {number2}
       </div>
       <div className='calculator-digits'>
-         {/**
-          * Enter here all of the MathOperation and DigitButton components
-          */}
+      {buttons.map(buttonValue =>
+       <DigitButton value={buttonValue}
+        onClick={!mathAction ?
+         () => addToNumber1(buttonValue):
+         () => addToNumber2(buttonValue)
+         } />)}
+      {operationTypes.map(operationType => {
+        
+        if(operationType === 'AC'){
+          return <MathOperation type={operationType} onClick={() => {
+            reset();
+          }} />
+        }
+        if(operationType === 'equal'){
+          return <MathOperation type={operationType}
+           onClick={() => handleEqualize()}
+         />
+        }
+        else{
+          return <MathOperation type={operationType}
+           onClick={(action) => {setMathAction(action)}} />
+        }
+      })}
       </div>
     </div>
   );
-}
+  }
 
 export default Calc;
